038ef2c237daf119490726391e2bfee50f6baed8
AlexandreBlais42 <95235429+AlexandreBlais42@users.noreply.github.com>
2022-08-23 23:27:37 -0400

    delay + pinMode
    fonction pinMode2 custom, INPUT_PULLUP pas encore fait, je dois aussi trouver comment overwrite pinMode

---
+++ lib/arduinoUnoRapide.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+//Temps
+void delay(int t){
+  _delay_ms(t);
+}
+
+//Pins
+
+void pinMode2(uint8_t pin, uint8_t mode){
+  //TODO: Trouver comment overwrite la fonction pinMode implémenter par arduino (wiring_digital.c)
+  volatile uint8_t *targeted_DDR = &DDRD;
+  
+  if (pin >= 8){
+    pin -= 8;
+    targeted_DDR = &DDRB;
+  }
+
+  pin = 1<<pin;
+  
+  if (mode == INPUT){
+    *targeted_DDR &= ~pin;
+  }else if (mode == OUTPUT){
+    *targeted_DDR |= pin;
+  }else{
+    //TODO: INPUT_PULLUP
+  }
+}
--- lib/arduinoUnoRapide.h
+++ lib/arduinoUnoRapide.h
@@ -1,1 +1,5 @@
+//Temps
+void delay(int t);
 
+//Pins
+void pinMode2(uint8_t pin, uint8_t mode);
