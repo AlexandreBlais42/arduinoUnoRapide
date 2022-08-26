0d65c80cbd373d1c87134b61dbbc4fb8fa16b356
AlexandreBlais42 <95235429+AlexandreBlais42@users.noreply.github.com>
2022-08-25 20:38:35 -0400

    Added digitalWrite2 and digitalRead2

--- lib/arduinoUnoRapide.cpp
+++ lib/arduinoUnoRapide.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
 //https://github.com/AlexandreBlais42/arduinoUnoRapide
 
-
 //Temps
 void delay(int t){
   _delay_ms(t);
 }
 
 //Pins
+void digitalWrite2(uint8_t pin, uint8_t mode){
+  //TODO: Trouver comment overwrite la fonction digitalWrite implémenter par arduino (wiring_digital.c)
+  volatile uint8_t * targeted_PORT = &PORTD;
+
+  if (pin >= 8){
+    pin -= 8;
+    targeted_PORT = &PORTB;
+  }
+
+  pin = 1<<pin; //pin devient un bit mask sans déclarer une nouvelle variable
+
+  if (mode == HIGH){
+    *targeted_PORT |= pin;
+  }else if (mode == LOW){
+    *targeted_PORT &= ~pin;
+  }
+}
 
 void pinMode2(uint8_t pin, uint8_t mode){
   //TODO: Trouver comment overwrite la fonction pinMode implémenter par arduino (wiring_digital.c)
@@ -18,13 +34,31 @@ void pinMode2(uint8_t pin, uint8_t mode){
     targeted_DDR = &DDRB;
   }
 
-  pin = 1<<pin;
+  uint8_t bitMask = 1<<pin;
   
   if (mode == INPUT){
-    *targeted_DDR &= ~pin;
+    *targeted_DDR &= ~bitMask;
   }else if (mode == OUTPUT){
-    *targeted_DDR |= pin;
+    *targeted_DDR |= bitMask;
+  }else if (mode == INPUT_PULLUP){
+    *targeted_DDR &= ~bitMask;
+    digitalWrite2(pin, HIGH);
+  }
+}
+
+uint8_t digitalRead2(uint8_t pin){
+  volatile uint8_t *targeted_PIN = &DDRD;
+
+  if (pin >= 8){
+    pin -= 8;
+    targeted_PIN = &DDRB;
+  }
+
+  pin = 1<<pin; //pin devient un bit mask sans déclarer une nouvelle variable
+
+  if (*targeted_PIN & pin){
+    return 1;
   }else{
-    //TODO: INPUT_PULLUP
+    return 0;
   }
 }
